6fd102aa6ccf2bbc26ecdf7cdb93b71aa1c56349
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-04 08:10:09 +0700

    create admin dashboard

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,27 @@ router.get('/admin', function (req, res) {
 
 // render manage order page
 router.get('/admin/manageorder', function (req, res) {
-  res.render('admin/manageorder', { title: 'Laragaa | Daftar Pesanan', isLogin: true });
+  res.render('admin/manageorder', { title: 'Laragaa | Admin Daftar Pesanan', isLogin: true });
 });
+
+// render category page
+router.get('/admin/category', function (req, res) {
+  res.render('admin/category', { title: 'Laragaa | Admin Kategori', isLogin: true });
+});
+
+// render admin product page
+router.get('/admin/product', function (req, res) {
+  res.render('admin/product', { title: 'Laragaa | Admin Produk', isLogin: true });
+});
+
+// render payment page
+router.get('/admin/payment', function (req, res) {
+  res.render('admin/payment', { title: 'Laragaa | Admin Pembayaran', isLogin: true });
+});
+
+// render customer page
+router.get('/admin/customer', function (req, res) {
+  res.render('admin/customer', { title: 'Laragaa | Admin Pelanggan', isLogin: true });
+});
+
 module.exports = router;
